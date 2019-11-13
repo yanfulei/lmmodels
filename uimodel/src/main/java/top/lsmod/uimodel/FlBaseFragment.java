13dcdfbfb1facdc7ea26fc6825281f9e1e7210b8
闫福磊 <yanfulei@foxmail.com>
2019-11-13 15:31:12 +0800

    增加基类

---
+++ uimodel/src/main/java/top/lsmod/uimodel/FlBaseFragment.java
@@ -0,0 +1,60 @@
+package top.lsmod.uimodel;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import top.lsmod.uimodel.base.FlBaseInterfaceReqBean;
+import top.lsmod.uimodel.base.FlBaseInterfaceRspBean;
+import top.lsmod.uimodel.base.IHttpFactory;
+import top.lsmod.uimodel.base.impl.OkHttpImpl;
+import top.lsmod.uimodel.utils.HttpUtils;
+
+public abstract class FlBaseFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        //设置布局
+        View root = inflater.inflate(initLayout(), container, false);
+        //初始化控件
+        initView();
+        //设置数据
+        initData();
+        return root;
+    }
+
+    abstract void initData();
+
+    abstract void initView();
+
+    abstract int initLayout();
+
+
+    /**
+     * 发送网络请求
+     *
+     * @param interfaceBean
+     */
+    public void sendRequest(String serverUrl, FlBaseInterfaceReqBean interfaceBean) {
+        IHttpFactory httpFactory = new OkHttpImpl();
+        if (interfaceBean.getInterfaceType().toLowerCase().contains("get")) {
+            String param = HttpUtils.parseURLPair(interfaceBean.getParam());
+            interfaceBean.setInterfaceName(interfaceBean.getInterfaceName() + "?" + param);
+            httpFactory.sendGet(serverUrl, interfaceBean, interfaceRspBean -> onNetWorkResponse(interfaceRspBean));
+        }
+    }
+
+    /**
+     * 网络请求返回信息
+     *
+     * @param interfaceRspBean
+     */
+    public void onNetWorkResponse(FlBaseInterfaceRspBean interfaceRspBean) {
+
+    }
+}
