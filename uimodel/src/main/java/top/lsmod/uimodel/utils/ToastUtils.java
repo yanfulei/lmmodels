a2f8243383c5c6a540e5754e08cb59710e6eb480
闫福磊 <yanfulei@foxmail.com>
2019-11-14 11:06:29 +0800

    增加toast

--- uimodel/src/main/java/top/lsmod/uimodel/utils/ToastUtils.java
+++ uimodel/src/main/java/top/lsmod/uimodel/utils/ToastUtils.java
@@ -2,8 +2,6 @@ package top.lsmod.uimodel.utils;
 
 import android.app.Activity;
 
-import com.sdsmdg.tastytoast.TastyToast;
-
 /**
  * Created by yanfulei on 2018/10/1
  * Email yanfulei1990@gmail.com
@@ -25,30 +23,8 @@ public class ToastUtils {
     public static void showToast(final Activity ctx, final String msg, int state) {
         // 判断是在子线程，还是主线程
         if ("main".equals(Thread.currentThread().getName())) {
-            swichState(ctx, msg, state);
         } else {
             // 子线程
-            ctx.runOnUiThread(() -> swichState(ctx, msg, state));
-        }
-    }
-
-
-    private static void swichState(Activity ctx, String msg, int stata) {
-        if (stata == ERROR) {
-            TastyToast.makeText(ctx, msg, TastyToast.LENGTH_SHORT, TastyToast.ERROR);
-            return;
-        }
-        if (stata == INFO) {
-            TastyToast.makeText(ctx, msg, TastyToast.LENGTH_SHORT, TastyToast.INFO);
-            return;
-        }
-        if (stata == WARNING) {
-            TastyToast.makeText(ctx, msg, TastyToast.LENGTH_SHORT, TastyToast.WARNING);
-            return;
-        }
-        if (stata == SUCCESS) {
-            TastyToast.makeText(ctx, msg, TastyToast.LENGTH_SHORT, TastyToast.SUCCESS);
-            return;
         }
     }
 }
